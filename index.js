7763301f3abeae6504d1d0b68469f9ce3c388d5e
sebastian <sebastian.calle@globant.com>
2024-11-27 22:58:56 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const { Readable, Writable } = require('stream');
+const { pipeline } = require('node:stream/promises')
+
+const app = express();
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
+app.use(express.static('public'));
+
+class MyReadableStream extends Readable {
+    constructor(options) {
+        super(options);
+        console.log(options)
+        this.chunkSize = options.chunkSize;
+        this.chunkQuantity = options.chunkQuantity;
+        this.counter = 0;
+    }
+
+    _read(size) {
+        if (this.counter < this.chunkQuantity) {
+            this.counter++;
+            const data = `Data chunk #${this.counter}\n`;
+            console.log(`Pushing: ${data.trim()}`);
+            const chunk = "1".repeat(this.chunkSize);
+            this.push(chunk);
+        } else {
+            this.push(null);
+        }
+    }
+}
+
+class MyWritableStream extends Writable {
+    constructor(options) {
+        super(options);
+        this.processing = false;
+        this.res = options.res
+    }
+
+    _write(chunk, encoding, callback) {
+        this.processing = true;
+        console.log(`Received: ${chunk.toString().trim()}`);
+        this.res.write('cr');
+        setTimeout(() => {
+            this.processing = false;
+            console.log(`Processed: ${chunk.toString().trim()}`);
+            callback();
+        }, 100);
+    }
+}
+
+app.get('/start-stream', async(req, res) => {
+    const highWaterMark = parseInt(req.query.hwm) || 2;
+    const chunkSize = parseInt(req.query.cs) || 1;
+    const chunkQuantity = parseInt(req.query.cq) || 10;
+
+    const writableStream = new MyWritableStream({ highWaterMark, res });
+    const readableStream = new MyReadableStream({ chunkQuantity, chunkSize });
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    pipeline(
+        readableStream,
+        writableStream
+    );
+
+    readableStream.on('data', (chunk) => {
+        console.log('adding data')
+        res.write('rsd')
+    })
+
+    writableStream.on('drain', (evt) => {
+        console.log('Writable stream is ready to accept more data.');
+        res.write('bc');
+    });
+
+    writableStream.on('finish', () => {
+        console.log('All data processed.');
+        res.end('fp');
+    });
+});
---
+++ public/script.js
@@ -0,0 +1,67 @@
+document.getElementById('startStream').addEventListener('click', () => {
+    const hwm = document.getElementById('hwm').value;
+    const cs = document.getElementById('cs').value;
+    const cq = document.getElementById('cq').value;
+
+    fetch(`/start-stream?hwm=${hwm}&cs=${cs}&cq=${cq}`)
+        .then(response => {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder("utf-8");
+            let counter = 0;
+            let chunksCount = 0;
+            let stopSignalsCount = 0;
+
+            function read() {
+                return reader.read().then(({ done, value }) => {
+                    if (done) {
+                        console.log("Stream finished.");
+                        return;
+                    }
+                    const responseCode = decoder.decode(value);
+                    if (responseCode.includes('rsd')) {
+                        const packagesNumber = (responseCode.match(/rsd/g) || []);
+                        document.querySelector('.packages__delivered').innerHTML = '';                        
+
+                        for(let i in packagesNumber) {
+                            const img = document.createElement("img");
+                            img.setAttribute('src', 'https://cdn.icon-icons.com/icons2/1465/PNG/512/724package_100522.png');
+                            img.setAttribute('height', 100);
+
+                            document.querySelector('.packages__delivered').appendChild(img);
+                        }
+                        const img = document.createElement("img");
+                        img.setAttribute('src', 'https://www.shutterstock.com/image-vector/stop-sign-icon-hand-vector-260nw-1549832570.jpg');
+                        img.setAttribute('height', 100);
+                        document.querySelector('.packages__delivered').appendChild(img);
+                        stopSignalsCount++;
+                        document.querySelector('.stop__signals').innerHTML = `stop signals: ${stopSignalsCount}`
+                    }
+                    if (responseCode.includes('cr')) {
+                        chunksCount++;
+                        const img = document.createElement("img");
+                        img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyV31uvIwvFvh-lRd-wYGFypMWCVXvRUbtfg&s');
+                        img.setAttribute('height', 100);    
+                        document.querySelector('.packages__processed').appendChild(img);
+                        document.querySelector('.chunks__count').innerHTML = `Chunks count: ${chunksCount}`
+                    }
+                    
+                    if (responseCode.includes('bc')) {
+                        document.querySelector('.packages__processed').innerHTML = '';
+                    }
+                    if (responseCode.includes('fp')) {
+                        const doneText = document.createElement("h1");
+                        doneText.innerText = "Done."
+                        document.querySelector('.packages__display').innerHTML = '';
+                        document.querySelector('.packages__display').appendChild(doneText);
+                    }
+                    
+                    return read();
+                });
+            }
+
+            return read();
+        })
+        .catch(error => {
+            console.error("Error in stream:", error);
+        });
+});
